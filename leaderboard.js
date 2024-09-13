6f9053efcb707840c4e66858f866616d6f048fef
Kbasius <kbasius@gmail.com>
2024-09-12 21:15:01 -0400

    Leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -1,14 +1,11 @@
-/*
 function loadLeaderboard() {
-    const usernames = JSON.parse(localStorage.getItem('usernames')) || []; 
-    const leaderboard = document.getElementById('leaderboard'); 
-
+    const usernames = JSON.parse(localStorage.getItem('usernames')) || [];
+    const leaderboard = document.getElementById('leaderboard');
     leaderboard.innerHTML = '';
-
     usernames.forEach(username => {
-        const li = document.createElement('li'); 
-        li.textContent = username; 
-        leaderboard.appendChild(li); 
+        const li = document.createElement('li');
+        li.textContent = username;
+        leaderboard.appendChild(li);
     });
 }
 
@@ -16,27 +13,4 @@ window.onload = loadLeaderboard;
 
 document.getElementById('backButton').addEventListener('click', function() {
     window.location.href = 'index.html';
-});
-*/
-
-
-
-
-
-function startGame(event) {
-    event.preventDefault();
-    const usernameInput = document.getElementById('username');
-    const username = usernameInput.value;
-    if (username) {
-        const usernames = JSON.parse(localStorage.getItem('usernames')) || [];
-        usernames.push(username);
-        localStorage.setItem('usernames', JSON.stringify(usernames));
-        
-        // Clear the input field
-        usernameInput.value = '';
-        
-        window.location.href = 'game.html';
-    } else {
-        alert("Please enter a username.");
-    }
-}
+  });
